d4a9a2773bb1d692f38940a3ad11c3740d9553fd
Joe Bush <joebush4444@gmail.com>
2024-08-24 00:20:02 -0400

    complete lab, finally!

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+
+function findMatching(array,string){
+    let matchingNames =[]
+    for(let item of array){
+        if(item.toUpperCase() === string.toUpperCase()){
+            matchingNames.push(item)
+        }
+    }
+    return matchingNames;
+}
+
+function fuzzyMatch(array,string){
+    let fuzMatch =[]
+    for(let item of array){
+        if(item.slice(0,2)===string.slice(0,2)){
+            fuzMatch.push(item);
+        }
+    }
+    return fuzMatch;
+}
+
+function matchName(array,string){
+    let matches =[];
+    for(let item of array){
+        if(item.name === string){
+            matches.push(item)
+        }
+    }
+    return matches;
+}
